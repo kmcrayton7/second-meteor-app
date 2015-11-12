15e4b4ff01d8611c464c6f03c6d517d15a2690f0
Kim Crayton <kmcrayton7@yahoo.com>
2015-11-12 15:44:04 -0500

    Add ability for user to add or remove items from todo list

--- client/client.js
+++ client/client.js
@@ -3,3 +3,27 @@ Template.todos.helpers({
         return Todos.find({}, {sort: {createdAt: -1}});
     }
 });
+
+Template.addTodo.events({
+   'submit form': function(event){
+       event.preventDefault();
+       var todoName = $('[name="todoName"]').val();
+       Todos.insert({
+           name: todoName,
+           completed: false,
+           createdAt: new Date()
+       });
+       $("[name='todoName']").val("");
+   }
+});
+
+Template.todoItem.events({
+    "click .delete-todo": function(event){
+        event.preventDefault();
+        var documentId = this._id;
+        var comfirm = window.confirm("Delete this task?");
+        if(confirm) {
+            Todos.remove({_id: documentId});
+        }
+    }
+});
