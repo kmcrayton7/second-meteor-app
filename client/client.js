aaaa7436e21d67f813df8cbf012fee24d6db2bb5
Kim Crayton <kmcrayton7@yahoo.com>
2015-11-27 11:02:09 -0500

    "Add route redirect feature"

--- client/client.js
+++ client/client.js
@@ -84,6 +84,8 @@ Template.addList.events({
         var listName = $('[name=listName]').val();
         Lists.insert({
             name: listName
+        }, function(error, results){
+            Router.go('listPage', { _id: results});
         });
         $('[name=listName]').val('');
     }
